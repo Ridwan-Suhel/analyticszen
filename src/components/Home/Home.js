8ddde69843791b05ed4fc63719c947f831373946
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-03 23:43:29 +0600

    home hero area added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,30 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello Home</h1>
-    </div>
+    <section>
+      <div className="container mx-auto ">
+        <div className="row flex gap-5 items-center h-[85vh]">
+          <div className="left-area w-7/12">
+            <h1 className="text-6xl font-black text-blue-800">
+              Make your life gorgeous with our glasses.
+            </h1>
+            <p className="text-xl my-7">
+              Let us help you to explore your colorful world with our futuristic
+              glasses. The modern and AI base glasses.
+            </p>
+            <button className="py-3 px-7 bg-blue-200 rounded-md text-blue-600 font-medium">
+              Live Demo
+            </button>
+          </div>
+          <div className="right-area w-5/12">
+            <img
+              src="https://templates.envytheme.com/busan/default/assets/img/eye-glass-image/1.png"
+              alt="glasses"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
