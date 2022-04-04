86c2d903af7b8169b30d6d388cb35000325af153
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-04-05 01:16:13 +0600

    dashboard added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,9 +8,13 @@ const Dashboard = () => {
       <h1 className="text-2xl font-bold my-10">Company Analytical Dashboard</h1>
       <div className="row ">
         <div className="line-chart w-6/12 h-[300px] mx-auto">
+          <h1 className="text-xl font-medium my-7">Company Revenue Chart</h1>
           <DashboardLineChart></DashboardLineChart>
         </div>
         <div className="pie-chart w-6/12 h-[300px] mx-auto my-10">
+          <h1 className="text-xl font-medium mt-20">
+            Company Revenue VS Investment
+          </h1>
           <DashboardPieChart></DashboardPieChart>
         </div>
       </div>
--- src/components/DashboardPieChart/DashboardPieChart.js
+++ src/components/DashboardPieChart/DashboardPieChart.js
@@ -4,52 +4,55 @@ import { Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 const DashboardPieChart = () => {
   const data01 = [
     {
-      month: "Jan",
-      investment: 100000,
-      sell: 241,
-    },
-    {
-      month: "Feb",
-      investment: 200000,
-      sell: 423,
-    },
-    {
       month: "Mar",
-      investment: 560000,
-      sell: 726,
+      investment: 100000,
     },
     {
       month: "Apr",
-      investment: 670000,
-      sell: 529,
+      investment: 200000,
+    },
+    {
+      month: "May",
+      investment: 500000,
+    },
+    {
+      month: "Jun",
+      investment: 500000,
+    },
+    {
+      month: "Jul",
+      investment: 600000,
+    },
+    {
+      month: "Aug",
+      investment: 700000,
     },
   ];
 
   const data02 = [
     {
+      month: "Mar",
+      revenue: 10401,
+    },
+    {
+      month: "Apr",
+      revenue: 24500,
+    },
+    {
       month: "May",
-      investment: 700000,
-      sell: 726,
+      revenue: 67010,
     },
     {
       month: "Jun",
-      investment: 400000,
-      sell: 529,
+      revenue: 40405,
     },
     {
       month: "Jul",
-      investment: 600000,
-      sell: 601,
+      revenue: 50900,
     },
     {
       month: "Aug",
-      investment: 700000,
-      sell: 670,
-    },
-    {
-      month: "Sep",
-      investment: 500000,
-      sell: 670,
+      revenue: 61000,
     },
   ];
   return (
@@ -65,7 +68,7 @@ const DashboardPieChart = () => {
         />
         <Pie
           data={data02}
-          dataKey="investment"
+          dataKey="revenue"
           cx="50%"
           cy="50%"
           innerRadius={70}
